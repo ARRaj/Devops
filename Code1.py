fac23891c46ea81db2950fe51041b84e8de9b22e
Arunraj Soundararajan <sarunraj311@gmail.com>
2023-12-19 11:51:16 +0530

    commit

---
+++ Code1.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+# Establish a connection to the MySQL database
+connection = mysql.connector.connect(
+    host="localhost",
+    user="",
+    password="",
+    database="DB"
+)
+
+# Create a cursor object to interact with the database
+cursor = connection.cursor()
+
+# Define your SQL query to retrieve data
+query = "SELECT * FROM table1"
+
+# Execute the query
+cursor.execute(query)
+
+# Fetch all the rows from the result set
+result = cursor.fetchall()
+
+# Display the retrieved data
+for row in result:
+    print(row)
+
+# Close the cursor and the database connection
+cursor.close()
+connection.close()
+
